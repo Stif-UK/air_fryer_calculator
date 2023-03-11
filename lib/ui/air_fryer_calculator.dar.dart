76de6f499c71e19e2113fe17e7785843130c768c
Grant Hutchison <stif@grants-mbp.lan>
2023-03-11 09:43:21 +0000

    Update page layout

--- lib/ui/air_fryer_calculator.dar.dart
+++ lib/ui/air_fryer_calculator.dar.dart
@@ -9,45 +9,61 @@ class AirFryerCalculator extends StatefulWidget {
 
 class _AirFryerCalculatorState extends State<AirFryerCalculator> {
   double temperature = 200;
-  double time = 0;
+  double time = 40;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Oven Temperature: ${temperature.toInt()}°C"),
-        Slider(
-          value: temperature,
-          min: 150,
-          max: 350,
-          onChanged: (double value) {
-            setState(()
-              => temperature = value);
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Text("Oven Temperature: ${temperature.toInt()}°C"),
+          Slider(
+            value: temperature,
+            min: 150,
+            max: 350,
+            onChanged: (double value) {
+              setState(()
+                => temperature = value);
+              },
+          divisions: 20,
+          label: "${temperature.toInt()}°C",),
+          //Text("${temperature.toInt()}°C"),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0,10,0,25),
+            child: Text("Oven Time: ${time.toInt()} minutes"),
+          ),
+          Slider(
+            value: time,
+            min: 0,
+            max: 180,
+            onChanged: (double value) {
+              setState(()
+              => time = value);
             },
-        divisions: 20,
-        label: "${temperature.toInt()}°C",),
-        //Text("${temperature.toInt()}°C"),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0,10,0,25),
-          child: Text("Oven Time: ${time.toInt()} minutes"),
-        ),
-        Slider(
-          value: time,
-          min: 0,
-          max: 180,
-          onChanged: (double value) {
-            setState(()
-            => time = value);
-          },
-          divisions: 36,
-          label: "${time.toInt()} mins",),
-        //Text("${time.toInt()} minutes")
-        const Divider(thickness: 2,),
-        const Text("Suggested Air Fryer Setting"),
-        Text("Temperature: ${temperature.toInt() - 20}"),
-        Text("Time: ${(time * 0.8).toInt()} minutes"),
+            divisions: 36,
+            label: "${time.toInt()} mins",),
+          //Text("${time.toInt()} minutes")
+          const Divider(thickness: 2,),
+          //Display the output
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Suggested Air Fryer Setting",style: Theme.of(context).textTheme.bodyLarge,),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text("Temperature: ${temperature.toInt() - 20}°C",
+            style: Theme.of(context).textTheme.displaySmall,
+            textAlign: TextAlign.center,),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Time:\n ${(time * 0.8).toInt()} minutes",
+            style: Theme.of(context).textTheme.displaySmall,
+            textAlign: TextAlign.center,),
+          ),
 
 
-      ]
+        ]
+      ),
     );
   }
 }
