df8c8f03dc983a77bf52ca059a6a6224c4ea30d4
Grant Hutchison <stif@grants-mbp.lan>
2023-04-04 17:52:32 +0100

    version bump and update to version history

--- lib/copy/version_history_copy.dart
+++ lib/copy/version_history_copy.dart
@@ -7,11 +7,16 @@ class VersionHistoryCopy{
     return const Padding(
       padding: EdgeInsets.all(12.0),
       child: Markdown(
-          data: "**Latest Version:**\n\n"
-              " **v1.0.0**\n\n"
+          data: "## Latest Version:\n"
+              "### v1.1.0\n"
+              "* Added ability to search notebook\n"
+              "* Added options to leave app reviews\n"
+              " --- \n"
+              "## Previous Releases:\n"
+              "### v1.0.0\n"
               "* Calculate reduced cooking temperature & time for Air Frying\n"
               "* Switch preference between Celsius and Fahrenheit\n"
-              "* Save notes for future reference\n\n"
+              "* Save notes for future reference\n"
               "* Edit saved notes"
 
 
