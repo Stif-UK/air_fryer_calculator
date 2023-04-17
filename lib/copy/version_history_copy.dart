fcfddfc5ea646abd805637e16f2adf0979a77cc3
Grant Hutchison <stif@grants-mbp.lan>
2023-04-17 23:41:12 +0100

    updated version history

--- lib/copy/version_history_copy.dart
+++ lib/copy/version_history_copy.dart
@@ -8,13 +8,17 @@ class VersionHistoryCopy{
       padding: EdgeInsets.all(12.0),
       child: Markdown(
           data: "## Latest Version:\n"
+              "### v1.2.0\n"
+              "* Added the ability to support the app and remove ads via in-app purchase\n"
+              "* Updates to sidebar menu\n"
+              " --- \n"
+              "## Previous Releases:\n"
               "### v1.1.0\n"
               "* Added ability to search notebook\n"
               "* Added options to leave app reviews\n"
               "* Updated validation on note fields\n"
               "* Additional performance enhancements\n"
               " --- \n"
-              "## Previous Releases:\n"
               "### v1.0.0\n"
               "* Calculate reduced cooking temperature & time for Air Frying\n"
               "* Switch preference between Celsius and Fahrenheit\n"
