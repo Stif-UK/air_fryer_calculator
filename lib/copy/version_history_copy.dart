13219206079cb1dbccb8562aab6ce16760bbd44f
Grant Hutchison <stif@grants-mbp.lan>
2023-04-29 09:21:25 +0100

    dynamic text footer for pro users

--- lib/copy/version_history_copy.dart
+++ lib/copy/version_history_copy.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:air_fryer_calculator/model/fryer_preferences.dart';
 
 class VersionHistoryCopy{
 
   static Widget getLatestVersionCopy(){
     return Markdown(
       physics: ScrollPhysics(),
-        data: "### v1.3.0\n"
+        data: _composeVersionCopy()
+
+        // "### v1.3.0\n"
+        // "* Added ability to filter the notebook by category\n"
+        // "* Implemented 'what's new' pop-up when updated\n\n"
+        // " --- \n\n"
+        //     "Enjoying Air Fryr? If you find the app useful please consider supporting it's continued development. \n\n"
+        //     "See 'Remove Ads' in the sidebar menu for more information"
+        );
+  }
+
+  static String _composeVersionCopy(){
+    bool isPro = FryerPreferences.getAppPurchasedStatus() ?? false;
+    String latestVersion = "### v1.3.0\n"
         "* Added ability to filter the notebook by category\n"
         "* Implemented 'what's new' pop-up when updated\n\n"
-        " --- \n\n"
-            "Enjoying Air Fryr? If you find the app useful please consider supporting it's continued development. \n\n"
-            "See 'Remove Ads' in the sidebar menu for more information"
-        );
+        " --- \n\n";
+
+    String proFooter = "Thank you for Supporting Air Fryr - your donation helps keep the app alive!";
+
+    String baseFooter = "Enjoying Air Fryr? If you find the app useful please consider supporting it's continued development. \n\n"
+        "See 'Remove Ads' in the sidebar menu for more information";
+
+    String footer = isPro? proFooter : baseFooter;
+
+    return latestVersion + footer;
+
   }
 
   static Widget getFullVersionHistory(){
