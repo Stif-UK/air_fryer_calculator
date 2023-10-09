37dc4a9d4c321301cd334a6db949b86e72735e70
Grant Hutchison <stif@grants-mbp.lan>
2023-10-09 12:32:18 +0100

    setting up framework to detect revenuecat triggered sales

--- lib/util/startup_helper.dart
+++ lib/util/startup_helper.dart
@@ -1,4 +1,7 @@
+import 'package:air_fryer_calculator/api/purchase_api.dart';
 import 'package:air_fryer_calculator/copy/version_history_copy.dart';
+import 'package:air_fryer_calculator/ui/sidebar_links/remove_ads.dart';
+import 'package:air_fryer_calculator/util/string_extention.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -24,7 +27,10 @@ class StartupChecksUtil{
 
       StartupChecksUtil.updateLatestVersion();
     }
-
+    //2. If not showing a what's new dialog, check if a sale dialog should show
+    else{
+      checkForSale();
+    }
       }
 
 
@@ -53,6 +59,34 @@ class StartupChecksUtil{
     String currentVersion = packageInfo.version.toString();
     FryerPreferences.setLatestVersion(currentVersion);
 
+  }
+
+  static checkForSale() async {
+    //fetchOffers only ever returns the a list with a single entry, the currently active offer
+    final offerings = await PurchaseApi.fetchOffers();
+    String? title;
+    String? description;
+    try {
+      //Try to access the title and description of the offer - if more than one entry has been returned this will throw an error
+      title = offerings.map((offer) => offer.identifier).single;
+      description = offerings.map((offer) => offer.serverDescription).single;
+      //if we successfully get the offer, check if the title includes the word 'sale' then display a dialog
+      if(title.containsSale){
+        Get.defaultDialog(
+          title: title,
+          content: Container(
+            child: Text(description),
+          ),
+          textConfirm: "Show me",
+          textCancel: "No Thanks",
+          onConfirm: () => Get.to(() => RemoveAds())
+        );
+      }
+    } on Error catch (e) {
+      print("Error caught trying to identify single RevenueCat offering: ${e.toString()}");
+    }
+
+    print(offerings.map((e) => e.serverDescription));
   }
 
   static showWhatsNewDialog(){
--- lib/util/string_extention.dart
+++ lib/util/string_extention.dart
@@ -4,4 +4,9 @@ extension extString on String{
     return titleRegExp.hasMatch(this);
   }
 
+  bool get containsSale{
+    final titleRegExp = RegExp(r'sale', caseSensitive: false);
+    return titleRegExp.hasMatch(this);
+  }
+
 }
