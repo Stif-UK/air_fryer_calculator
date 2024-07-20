750edb1b9e8fb22162bda6dd25bfd3a109c28790
Grant Hutchison <stif@grants-mbp.lan>
2024-07-20 13:44:29 +0100

    git status

--- lib/controller/LanguageController.dart
+++ lib/controller/LanguageController.dart
@@ -1,11 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:air_fryer_calculator/model/fryer_preferences.dart';
 
 class LanguageController extends GetxController{
-  final locale = FryerPreferences.getLocalePreference().obs;
-
-  updateLocalePref(String value){
-    FryerPreferences.setLocalePreference(value);
+  final locale = Locale(FryerPreferences.getLocalePreference()).obs;
+  
+  updateLocalePref(Locale value){
+    FryerPreferences.setLocalePreference(value.toString());
     locale(value);
   }
 
