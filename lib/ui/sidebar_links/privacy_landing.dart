f1ff25030364cc2f93a8b764ffd7e585f13c8366
Grant Hutchison <stif@grants-mbp.lan>
2024-07-20 18:38:53 +0100

    updating languages

--- lib/ui/sidebar_links/privacy_landing.dart
+++ lib/ui/sidebar_links/privacy_landing.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:air_fryer_calculator/privacy/initialisation_helper.dart';
 import 'package:air_fryer_calculator/ui/sidebar_links/privacy_policy.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 class PrivacyLanding extends StatefulWidget {
   const PrivacyLanding({super.key});
@@ -16,17 +18,17 @@ class _PrivacyLandingState extends State<PrivacyLanding> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Privacy"),
+        title: Text(AppLocalizations.of(context)!.privacy),
       ),
       body: Column(
         children: [
-          ListTile(title: const Text("Privacy Policy"),
+          ListTile(title: Text(AppLocalizations.of(context)!.privacyPolicy),
           leading: const Icon(Icons.privacy_tip_outlined),
           onTap: (){
             Get.to(() => PrivacyPolicy());
           }),
           const Divider(thickness: 2,),
-          ListTile(title: const Text("Privacy Settings"),
+          ListTile(title: Text(AppLocalizations.of(context)!.privacySettings),
           leading: const Icon(Icons.settings),
           onTap: () async{
             final scaffoldMessenger = ScaffoldMessenger.of(context);
