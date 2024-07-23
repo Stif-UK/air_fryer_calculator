e98437a063c6f73243e044aedb2963fe1f98a74f
Grant Hutchison <stifdev.uk@gmail.com>
2024-07-23 20:00:54 +0100

    implement ad on app_data page

--- lib/model/adUnitsDEMO.dart
+++ lib/model/adUnitsDEMO.dart
@@ -38,5 +38,8 @@ class AdUnitsDEMO{
       ? "ca-app-pub-3940256099942544/6300978111"
       : "ca-app-pub-3940256099942544/2934735716";
 
+  static String get appdataAdUnitID => Platform.isAndroid
+      ? "ca-app-pub-3940256099942544/6300978111"
+      : "ca-app-pub-3940256099942544/2934735716";
 
 }
--- lib/ui/sidebar_links/app_data.dart
+++ lib/ui/sidebar_links/app_data.dart
@@ -1,8 +1,14 @@
+import 'package:air_fryer_calculator/model/adUnits.dart';
+import 'package:air_fryer_calculator/model/fryer_preferences.dart';
+import 'package:air_fryer_calculator/provider/adstate.dart';
 import 'package:air_fryer_calculator/ui/app_data/restore.dart';
 import 'package:air_fryer_calculator/ui/app_data/share_backup.dart';
+import 'package:air_fryer_calculator/util/ad_widget_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
 
 
 class AppData extends StatefulWidget {
@@ -13,6 +19,30 @@ class AppData extends StatefulWidget {
 }
 
 class _AppDataState extends State<AppData> {
+  BannerAd? banner;
+  bool purchaseStatus = FryerPreferences.getAppPurchasedStatus() ?? false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if(!purchaseStatus)
+    {
+      final adState = Provider.of<AdState>(context);
+      adState.initialization.then((status) {
+        setState(() {
+          banner = BannerAd(
+              adUnitId: AdUnits.appdataAdUnitID,
+              //If the device screen is large enough display a larger ad on this screen
+              size: AdSize.banner,
+              request: const AdRequest(),
+              listener: adState.adListener)
+            ..load();
+        });
+      });
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,66 +50,74 @@ class _AppDataState extends State<AppData> {
         title: Text(AppLocalizations.of(context)!.appData),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 30,),
-            SizedBox(
-              width: (MediaQuery.of(context).size.width)*0.8,
-              height: (MediaQuery.of(context).size.height)*0.15,
-              child: ElevatedButton(
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text("Backup",
-                      style: Theme.of(context).textTheme.headlineLarge
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 30,),
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width)*0.8,
+                    height: (MediaQuery.of(context).size.height)*0.15,
+                    child: ElevatedButton(
+                        child: Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text("Backup",
+                            style: Theme.of(context).textTheme.headlineLarge
+                          ),
+                        ),
+                        onPressed: (){
+                          Get.to(() => ShareBackup());
+                        },
+                        style: ButtonStyle(
+                            backgroundColor: Get.isDarkMode? WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.inversePrimary) : WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.primary),
+                            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                    side: const BorderSide(color: Colors.black)
+                                )
+
+                            )
+                        )
                     ),
                   ),
-                  onPressed: (){
-                    Get.to(() => ShareBackup());
-                  },
-                  style: ButtonStyle(
-                      backgroundColor: Get.isDarkMode? WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.inversePrimary) : WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.primary),
-                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                              side: const BorderSide(color: Colors.black)
-                          )
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: (MediaQuery.of(context).size.width)*0.8,
+                      height: (MediaQuery.of(context).size.height)*0.15,
+                      child: ElevatedButton(
+                          child: Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text("Restore",
+                              style: Theme.of(context).textTheme.headlineLarge
+                            ),
+                          ),
+                          onPressed: (){
+                            Get.to(() => const Restore());
+                          },
+                          style: ButtonStyle(
+                              backgroundColor: Get.isDarkMode? WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.inversePrimary) : WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.primary),
+                              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      side: const BorderSide(color: Colors.black)
+                                  )
 
-                      )
-                  )
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: SizedBox(
-                width: (MediaQuery.of(context).size.width)*0.8,
-                height: (MediaQuery.of(context).size.height)*0.15,
-                child: ElevatedButton(
-                    child: Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Text("Restore",
-                        style: Theme.of(context).textTheme.headlineLarge
+                              )
+                          )
                       ),
                     ),
-                    onPressed: (){
-                      Get.to(() => const Restore());
-                    },
-                    style: ButtonStyle(
-                        backgroundColor: Get.isDarkMode? WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.inversePrimary) : WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.primary),
-                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                                side: const BorderSide(color: Colors.black)
-                            )
-
-                        )
-                    )
-                ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ));
+          ),
+          purchaseStatus? const SizedBox(height: 0,) : AdWidgetHelper.buildRectangleSpace(banner, context),
+          const SizedBox(height: 50,)
+        ],
+      ));
   }
 }
