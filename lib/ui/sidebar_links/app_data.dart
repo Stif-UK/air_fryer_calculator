5e7b5643d044a2f1966c42f301e8c156a59ef185
Grant Hutchison <stifdev.uk@gmail.com>
2024-07-23 18:43:29 +0100

    setup buttons

--- lib/ui/sidebar_links/app_data.dart
+++ lib/ui/sidebar_links/app_data.dart
@@ -17,8 +17,69 @@ class _AppDataState extends State<AppData> {
         title: Text(AppLocalizations.of(context)!.appData),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30,),
+            SizedBox(
+              width: (MediaQuery.of(context).size.width)*0.8,
+              height: (MediaQuery.of(context).size.height)*0.15,
+              child: ElevatedButton(
+                  child: const Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text("Backup",
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),),
+                  ),
+                  onPressed: (){
+                    //Get.to(() => ShareBackup());
+                    //Platform.isIOS? Get.to(()=> const ShareBackup()) :Get.to(() => const Backup());
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.inversePrimary),
+                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                              side: const BorderSide(color: Colors.black)
+                          )
 
-      ),
-    );
+                      )
+                  )
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: (MediaQuery.of(context).size.width)*0.8,
+                height: (MediaQuery.of(context).size.height)*0.15,
+                child: ElevatedButton(
+                    child: const Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Text("Restore",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),),
+                    ),
+                    onPressed: (){
+                      //Get.to(() => const Restore());
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(Theme.of(context).buttonTheme.colorScheme!.inversePrimary),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                                side: const BorderSide(color: Colors.black)
+                            )
+
+                        )
+                    )
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
